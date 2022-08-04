a8861901bdc48b27ea77eddcb1293abfdbe9c940
piuu-piuu <91653435+piuu-piuu@users.noreply.github.com>
2022-08-04 12:06:35 +0500

    fixed var names

--- examples/dynamic.rs
+++ examples/dynamic.rs
@@ -5,11 +5,11 @@ fn main() {
     let mut composite_shape = Shape { points: Vec::new() };
 
     for _ in 1..10 {
-        let boxedpoint = Point {
+        let point = Point {
             x: rand::thread_rng().gen_range(1..101),
             y: rand::thread_rng().gen_range(1..101),
         };
-        composite_shape.points.push(boxedpoint);
+        composite_shape.points.push(point);
     }
 
     let mut scene: Vec<Box<dyn Geometry>> = Vec::new();
